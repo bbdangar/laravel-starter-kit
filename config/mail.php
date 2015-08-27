eb194ffc2ee8d70c904e0592115a4d8a7cfe9b6c
Daksh Mehta <daksh@dristal.com>
2015-08-28 02:59:27 +0530

    Sender email address is Set

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'someemail@gmail.com', 'name' => 'Some Name'],
 
 	/*
 	|--------------------------------------------------------------------------
